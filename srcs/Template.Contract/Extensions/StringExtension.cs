eef02bb1e374e4a2f27698ad6d415d6d8b75493b
Elias Bui <thaibdtg16@gmail.com>
2024-01-10 15:26:29 +0700

    New extension for string compose

---
+++ srcs/Template.Contract/Extensions/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Template.Contract.Extensions;
+
+public static class StringExtension
+{
+    public static string ListToArrayString(this List<string>? inputList)
+    {
+        if (inputList == null || !inputList.Any()) return "''";
+        return string.Join(",", inputList.Select(x => "'" + x + "'"));
+    }
+    public static string ListToArrayString(this List<Guid>? inputList)
+    {
+        if (inputList == null || !inputList.Any()) return "''";
+        return string.Join(",", inputList.Select(x => "'" + x + "'"));
+    }
+}
