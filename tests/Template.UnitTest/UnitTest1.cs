cc47dbd49d68b7cc0738d369a2480e2b43db8683
Elias Bui <thaibdtg16@gmail.com>
2024-01-10 15:30:25 +0700

    🔕 add new project test

---
+++ tests/Template.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Template.UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
